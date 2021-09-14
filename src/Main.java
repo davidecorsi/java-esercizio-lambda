f7d5f9205dbdfb5cd766f6dffae67d309a774434
Davide Corsi <davide.corsi@par-tec.it>
2021-09-14 14:42:12 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/*
+ * Eliminate dall'array i numeri dispari, modificate ogni valore moltiplicando se stesso per 2 e stampate il
+ * risultato. Utilizzate le lambda expression ed i reference method dove possibile.
+ * forEach(Consumer<? super T> action)
+ * removeIf(Predicate<? super E> filter)
+ * replaceAll(UnaryOperator<E> operator)
+ */
+public class Main {
+	public static void main(String[] args) {
+		List<Integer> integerList = new ArrayList<>(Arrays.asList(2, 5, 8, 10, 20, 15, 35, 15, 8, 12, 3));
+		// versione con le lambda
+		integerList.removeIf(n -> n % 2 == 1);
+		integerList.replaceAll(n -> n * 2);
+		integerList.forEach(System.out::println);
+		List<Integer> integerList1 = new ArrayList<>(Arrays.asList(2, 5, 8, 10, 20, 15, 35, 15, 8, 12, 3));
+	}
+}
